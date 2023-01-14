e8d3696fada512c29a11cdb54327ea2678abc6eb
Dimas Prabowo <98381997+dimgthb@users.noreply.github.com>
2023-01-14 08:54:15 +0700

    update

---
+++ go.mod
@@ -0,0 +1,38 @@
+module practice
+
+go 1.19
+
+require (
+	github.com/gin-contrib/sse v0.1.0
+	github.com/gin-gonic/gin v1.8.2
+	github.com/go-gorp/gorp/v3 v3.1.0
+	github.com/go-playground/locales v0.14.1
+	github.com/go-playground/universal-translator v0.18.0
+	github.com/go-playground/validator/v10 v10.11.1
+	github.com/gobuffalo/logger v1.0.7
+	github.com/gobuffalo/packd v1.0.2
+	github.com/gobuffalo/packr/v2 v2.8.3
+	github.com/goccy/go-json v0.10.0
+	github.com/joho/godotenv v1.4.0
+	github.com/json-iterator/go v1.1.12
+	github.com/karrick/godirwalk v1.17.0
+	github.com/leodido/go-urn v1.2.1
+	github.com/lib/pq v1.10.7
+	github.com/markbates/errx v1.1.0
+	github.com/markbates/oncer v1.0.0
+	github.com/markbates/safe v1.0.1
+	github.com/mattn/go-isatty v0.0.17
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd
+	github.com/modern-go/reflect2 v1.0.2
+	github.com/pelletier/go-toml/v2 v2.0.6
+	github.com/rubenv/sql-migrate v1.3.0
+	github.com/sirupsen/logrus v1.9.0
+	github.com/ugorji/go/codec v1.2.8
+	golang.org/x/crypto v0.5.0
+	golang.org/x/net v0.5.0
+	golang.org/x/sys v0.4.0
+	golang.org/x/term v0.4.0
+	golang.org/x/text v0.6.0
+	google.golang.org/protobuf v1.28.1
+	gopkg.in/yaml.v2 v2.4.0
+)
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"practice/controllers"
+	"practice/database"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "admin1"
+	dbname   = "practice"
+)
+
+var (
+	DB  *sql.DB
+	err error
+)
+
+func main() {
+	err = godotenv.Load("config/.env")
+	if err != nil {
+		fmt.Println("failed load file environment")
+	} else {
+		fmt.Println("succes read file environment")
+	}
+
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv(`DB_HOST`), os.Getenv(`DB_PORT`), os.Getenv(`DB_USER`), os.Getenv(`DB_PASSWORD`), os.Getenv(`DB_NAME`))
+	// psqlInfo := fmt.Sprintf("host=#{os.Getenv(`DB_HOST`)} port=#{os.Getenv(`DB_PORT`)} user=#{os.Getenv(`DB_USER`)} password=#{os.Getenv(`DB_PASSWORD`)} dbname=#{os.Getenv(`DB_NAME`)} sslmode=disable")
+
+	DB, err = sql.Open("postgres", psqlInfo)
+	err = DB.Ping()
+	if err != nil {
+		fmt.Println("DB Connection Failed")
+		panic(err)
+	} else {
+		fmt.Println("DB Connection Success")
+	}
+
+	database.DbMigrate(DB)
+	defer DB.Close()
+
+	// Router GIN
+	router := gin.Default()
+	router.GET("/persons", controllers.GetAllPerson)
+	router.POST("/persons", controllers.InsertPerson)
+	router.PUT("/persons/:id", controllers.UpdatePerson)
+	router.DELETE("/persons/:id", controllers.DeletePerson)
+
+	router.Run("localhost:8080")
+}
